08e285450fdf1f739cf3724554bad63e24f7edf3
Taiwo Adeyemo <taiwoadex96@gmail.com>
2026-05-16 08:17:21 +0100

    Utils added

--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,17 @@
 import os
 import sys
-import dill
+import joblib
 
 from src.exception import CustomException
-
+from src.logger import logging
 
 def save_object(file_path, obj):
+    """Saves a python object to a specified file path using joblib."""
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
-
-        with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
-
-    except Exception as e:
-        raise CustomException(e, sys)
-
-
-def load_object(file_path):
-    try:
-        with open(file_path, "rb") as file_obj:
-            return dill.load(file_obj)
-
+        
+        joblib.dump(obj, file_path)
+        logging.info(f"Successfully saved object at: {file_path}")
     except Exception as e:
         raise CustomException(e, sys)
