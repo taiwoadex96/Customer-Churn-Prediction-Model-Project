8f80d2e6eed0af0c09a053f9182ea8f702dce5ab
Taiwo Adeyemo <taiwoadex96@gmail.com>
2026-05-16 07:47:02 +0100

    Logger folder created

--- src/logger.py
+++ src/logger.py
@@ -11,7 +11,7 @@ os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
-# Configure logging
+# Create Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
